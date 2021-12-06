a926914ce1dfbe6d67b7809ed1789e3e27f1dc43
fchoi1 <masterfabster2010@hotmail.com>
2021-12-05 22:03:11 -0500

    added to generate webpage

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2,process.argv.length);
+const [name, github] = profileDataArgs;
 
-
-const printProfileData = (profileDataArr) => {
-
-    for (data of profileDataArr) console.log(data);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
     
-
-    profileDataArr.forEach( profileItem => console.log(profileItem));
-        
-    
-}
-printProfileData(profileDataArgs);
-
+    console.log("Portfolio complete! Check out index.html to see the output!");
+})
